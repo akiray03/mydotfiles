25f6d5362990f2e6df85f6b9975f3488c94494ba
Akira Yumiyama <yumiyama.akira@gmail.com>
2015-03-05 23:11:45 +0900

    Xcode check @ prepare.sh

--- provision/prepare.sh
+++ provision/prepare.sh
@@ -4,10 +4,16 @@ cd $(dirname ${BASH_SOURCE:-$0})
 source ../functions/command_exist.sh
 
 if [ $(uname -s) == 'Darwin' ]; then
-	if ! command_exist brew; then
-		echo 'Installing Homebrew...'
-		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-		brew tap homebrew/dupes
-		brew tap homebrew/binary
-	fi
+  if ! command_exist xcode-select; then
+    echo 'Please install Xcode first.'
+    open 'https://itunes.apple.com/jp/app/xcode/id497799835'
+    exit 1
+  fi
+
+  if ! command_exist brew; then
+    echo 'Installing Homebrew...'
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew tap homebrew/dupes
+    brew tap homebrew/binary
+  fi
 fi
