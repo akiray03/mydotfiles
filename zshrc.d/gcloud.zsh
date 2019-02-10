48b52e250bc4ab98dc8041327b6edaa7971d4952
Akira Yumiyama <yumiyama.akira@gmail.com>
2019-02-11 01:09:13 +0900

    CLOUDSDK_PYTHON を宣言する

--- zshrc.d/gcloud.zsh
+++ zshrc.d/gcloud.zsh
@@ -4,3 +4,7 @@ fi
 if [ -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc ]; then
   source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 fi
+
+if [ -f ~/.pyenv/versions/2.7.13/bin/python ]; then
+  export CLOUDSDK_PYTHON=~/.pyenv/versions/2.7.13/bin/python
+fi
